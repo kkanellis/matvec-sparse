7621d7a20afa9921f89f532fd6b3a0d4d90f7e4f
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-01-15 15:01:44 +0200

    add util library for common functions
    - defines `debug` macro
    - implements random vector func

---
+++ util.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ * Generate a random real vector of size N with
+ * elements beloning to [0, limit)
+ */
+void random_vec (double *v, int N, int limit)
+{
+    /* fill v with random doubles */
+    limit--;
+    srand( 410 );
+    for (int i = 0; i < N; i++) {
+        v[i] = ((double)rand()) / (((double)RAND_MAX) / limit) + ((double)rand()) / ((double)RAND_MAX);
+    }
+}
+
---
+++ util.h
@@ -0,0 +1,15 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+/* define useful debugging print macro */
+#ifdef DEBUG
+    #define debug(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__);
+#else
+    #define debug(...) do ; while(0)
+#endif
+
+#include "util.c"
+
+void random_vec(double *v, int N, int limit);
+
+#endif
