685c30b6616b2e737a7dab8c670f7e220227eb82
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-01-18 20:40:04 +0200

    major refactoring
    - move parallel code to new function
    - merge usefull variables (N, NZ, nelements etc.) to a struct entry to save communication costs

--- matvec_mpi_rows.c
+++ matvec_mpi_rows.c
@@ -14,47 +14,119 @@
 
 // TODO: replace broadcast with point-to-point for x vector
 
-void mat_vec_mult(const double *values,
-                    const int *i_idx,
-                    const int *j_idx,
-                    const double *x,
-                    double *y,
-                    const int NZ,
-                    const int row_offset)
+typedef struct {
+    int N,              /* dim of matrix */
+        NZ,             /* number of non-zero elements */
+        nz_count,       /* number of matrix elements for each process */
+        nz_start_idx,   /* first matrix element for each process */
+        row_count,      /* number of rows to process for each process */
+        row_start_idx;  /* first row processed for each process */ 
+} proc_info_t;
+MPI_Datatype proc_info_type;
+
+
+double* mat_vec_mult_parallel(int rank, int nprocs, proc_info_t *all_proc_info,
+                            int *i_idx, int *j_idx, double *values, double *x)
 {
-    for (int k = 0 ; k < NZ; k++) {
-        y[ i_idx[k] - row_offset ] += values[k] * x[ j_idx[k] ];
+    proc_info_t proc_info;
+    double *res;
+
+    int *count, *offset;
+
+    /* scatter to processors all info that will be needed */
+    MPI_Scatter(all_proc_info, 1, proc_info_type, &proc_info,
+                    1, proc_info_type, MASTER, MPI_COMM_WORLD);
+
+    /* allocate memory for vectors and submatrixes */
+    double *y = (double *)calloc_or_exit( proc_info.row_count, sizeof(double) );
+    if (rank != MASTER) {
+        x = (double *)malloc_or_exit( proc_info.N * sizeof(double) );
+        i_idx = (int *)malloc_or_exit( proc_info.nz_count * sizeof(int) );
+        j_idx = (int *)malloc_or_exit( proc_info.nz_count * sizeof(int) );
+        values = (double *)malloc_or_exit( proc_info.nz_count * sizeof(double) );
     }
+    else {
+        res = (double *)malloc_or_exit( proc_info.N * sizeof(double) );
+    }
+
+    debug("[%d] %d %d %d\n", rank, proc_info.N, proc_info.NZ, proc_info.nz_count);
+    
+    /* broadcast x vector */
+    /* TODO: change late to Point-2-Point communication */
+    MPI_Bcast(x, proc_info.N, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+    /* scatter matrix elements to processes */
+    if (rank == MASTER) {
+        count = (int *)malloc_or_exit( nprocs * sizeof(int) );
+        offset = (int *)malloc_or_exit( nprocs * sizeof(int) );
+        for (int p = 0; p < nprocs; p++) {
+            count[p] = all_proc_info[p].nz_count;
+            offset[p] = all_proc_info[p].nz_start_idx;
+        }
+    }
+    MPI_Scatterv(i_idx, count, offset, MPI_INT, i_idx, 
+                    proc_info.nz_count, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Scatterv(j_idx, count, offset, MPI_INT, j_idx, 
+                    proc_info.nz_count, MPI_INT, MASTER, MPI_COMM_WORLD);
+    MPI_Scatterv(values, count, offset, MPI_DOUBLE, values, 
+                    proc_info.nz_count, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+    /* validation check */
+    for (int i = 0; i < proc_info.nz_count; i++) {
+        debug("[%d] %d %d %lf\n", rank, i_idx[i], j_idx[i], values[i]);
+    }
+
+    /* multiplication kernel */
+    for (int k = 0 ; k < proc_info.nz_count; k++) {
+        y[ i_idx[k] - proc_info.row_start_idx ] += values[k] * x[ j_idx[k] ];
+    }
+
+    /* gather y elements from processes and save it to res */
+    if (rank == MASTER) {
+        res = (double *)malloc_or_exit( proc_info.N * sizeof(double) );
+        for (int p = 0; p < nprocs; p++) {
+            count[p] = all_proc_info[p].row_count;
+            offset[p] = all_proc_info[p].row_start_idx;
+        }
+    }
+    MPI_Gatherv(y, proc_info.row_count, MPI_DOUBLE, res, count, 
+                offset, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
+
+    /* return the result of multiplication */
+    return res;
 }
 
 /* 
  * Partition non-zero matrix entries to (almost)
  * equal number of rows for each process
  */
-void partition_equal_rows(int N, int NZ, int nprocs, int *row_idx, int *nz_count, 
-                            int *nz_start_idx, int *row_count, int *row_start_idx)
+void partition_equal_rows(proc_info_t *proc_info, int nprocs, const int *row_idx)
 {
-    int last_row, i = 0;
-    double row_chunk = ((double)N) / nprocs; /* rows for each process */
+    /* rows for each process */
+    double row_chunk = ((double)proc_info[0].N) / nprocs; 
 
+    int last_row, i = 0;
     for (int k = 0; k < nprocs; k++) {
-        nz_count[k] = 0;
-        nz_start_idx[k] = i;
+        proc_info[k].nz_count = 0;
+        proc_info[k].nz_start_idx = i;
 
         last_row = (int)((k + 1) * row_chunk - 1);
 
-        row_start_idx[k] = (int)(k * row_chunk);
-        row_count[k] = last_row - row_start_idx[k] + 1;
+        proc_info[k].row_start_idx = (int)(k * row_chunk);
+        proc_info[k].row_count = last_row - proc_info[k].row_start_idx + 1;
 
-        while (i < NZ && row_idx[i] <= last_row) {
-            nz_count[k]++; i++;
+        while (i < proc_info[0].NZ && row_idx[i] <= last_row) {
+            proc_info[k].nz_count++; 
+            i++;
         }
 
-        debug("[%d] row\t %d %d\n", k, row_count[k], row_start_idx[k]);
-        debug("[%d] nz\t %d %d\n", k, nz_count[k], nz_start_idx[k]);
+        debug("[%d] row\t %d %d\n", k, proc_info[k].row_count, 
+                                        proc_info[k].row_start_idx);
+        debug("[%d] nz\t %d %d\n", k, proc_info[k].nz_count, 
+                                        proc_info[k].nz_start_idx);
     }
     /* add remaining elements (if any) to last task */
-    nz_count[nprocs - 1] += NZ - i;  
+    proc_info[nprocs - 1].nz_count += proc_info[0].NZ - i;  
 }
 
 /* 
@@ -65,31 +137,35 @@ void partition_equal_nz_elements() {
     /* Not implemented */
 }
 
+/*
+ * Creates two MPI derived datatypes for the respective structs
+ */
+void create_mpi_datatypes(MPI_Datatype *proc_info_type) {
+    MPI_Datatype oldtypes[2];
+    MPI_Aint offsets[2], extent;
+    int blockcounts[2];
+
+    /* create `proc_info_t` datatype */
+    offsets[0] = 0;
+    oldtypes[0] = MPI_INT;
+    blockcounts[0] = 6;
+
+    MPI_Type_create_struct(1, blockcounts, offsets, oldtypes, proc_info_type);
+    MPI_Type_commit(proc_info_type);
+}
 
 int main(int argc, char * argv[])
 {
     char *in_file,
          *out_file = NULL;
 
-    double *values; /* a_values array */
-    int *i_idx,     /* i_index array */
-        *j_idx;     /* j_index array */
-
-    double *x, *y;  /* Ax = y */
-    int N,          /* dim of matrix */
-        NZ;         /* number of non-zero elements */
+    double *x; /* vector to be multiplied */
 
     int nprocs,     /* number of tasks/processes */
-        rank,       /* id of task/process */
-        nelements,  /* elements stored in this process */
-        nrows,      /* rows stored in this process */
-        first_row;  /* starting row for this process */
-
+        rank;       /* id of task/process */
+    
     /***** MPI MASTER (root) process only ******/
-    int *nz_start_idx,  /* first matrix element for each process */
-        *nz_count,      /* number of matrix elements for each process */
-        *row_start_idx, /* first row processed for each process */ 
-        *row_count;     /* number of rows to process for each process */
+    proc_info_t *proc_info;
 
     int *buf_i_idx,     /* row index for all matrix elements */
         *buf_j_idx;     /* column index for all matrix elements */
@@ -98,11 +174,13 @@ int main(int argc, char * argv[])
            *res;        /* final result -> Ax */
 
     /*******************************************/
-    
+
     /* Initialize MPI */
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    create_mpi_datatypes(&proc_info_type);
 
     /* master thread reads matrix */
     if (rank == MASTER) {
@@ -116,126 +194,83 @@ int main(int argc, char * argv[])
             if (argc == 3) 
                 out_file = argv[2];
         }
+
+        /* initialize proc_info array */
+        proc_info = (proc_info_t *)malloc_or_exit( nprocs * sizeof(proc_info_t) );
         
         /* read matrix */
-        if ( read_matrix(in_file, &buf_i_idx, &buf_j_idx, 
-                                    &buf_values, &N, &NZ) != 0) {
+        if ( read_matrix(in_file, &buf_i_idx, &buf_j_idx, &buf_values,
+                            &proc_info[MASTER].N, &proc_info[MASTER].NZ) != 0) {
             fprintf(stderr, "read_matrix: failed\n");
             exit(EXIT_FAILURE);
         }
 
         debug("Read matrix from '%s'!\n", in_file);
-        debug("[%d]: Matrix properties: N = %d, NZ = %d\n",rank, N, NZ);
+        debug("[%d]: Matrix properties: N = %d, NZ = %d\n",rank, 
+                                    proc_info[MASTER].N, proc_info[MASTER].NZ);
+
+        /* initialize process info */
+        for (int p = 0; p < nprocs; p++) {
+            if (p != MASTER) {
+                proc_info[p] = proc_info[MASTER];
+
+                debug("[%d] %d\n", p, proc_info[p].N);
+            }
+        }
 
         /* allocate x, res vector */
-        buf_x = (double *)malloc_or_exit( N * sizeof(double) );
-        res = (double *)malloc_or_exit( N * sizeof(double) );
+        buf_x = (double *)malloc_or_exit( proc_info[MASTER].N * sizeof(double) );
+        res = (double *)malloc_or_exit( proc_info[MASTER].N * sizeof(double) );
 
         /* generate random vector */
         //random_vec(buf_x, N, MAX_RANDOM_NUM);
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < proc_info[MASTER].N; i++) {
             buf_x[i] = 1;
         }
 
-        /* allocate memory */
-        nz_count = (int *)malloc_or_exit( nprocs * sizeof(int) );
-        nz_start_idx = (int *)malloc_or_exit( nprocs * sizeof(int) );
-        row_count = (int *)malloc_or_exit( nprocs * sizeof(int) );
-        row_start_idx = (int *)malloc_or_exit( nprocs * sizeof(int) );
-
         /* divide work across processes */
-        partition_equal_rows(N, NZ, nprocs, buf_i_idx, nz_count, 
-                                nz_start_idx, row_count, row_start_idx);
+        partition_equal_rows(proc_info, nprocs, buf_i_idx);
     }
 
-    /* broadcast matrix properties N, NZ */
-    MPI_Bcast(&N, 1, MPI_INT, MASTER, MPI_COMM_WORLD);
-    MPI_Bcast(&NZ,1, MPI_INT, MASTER, MPI_COMM_WORLD);
-
-    /* scatter number of rows each process is responsible for */
-    MPI_Scatter(row_count, 1, MPI_INT, &nrows, 
-                1, MPI_INT, MASTER, MPI_COMM_WORLD);
-    /* scatter starting row for each process */
-    MPI_Scatter(row_start_idx, 1, MPI_INT, &first_row, 
-                1, MPI_INT, MASTER, MPI_COMM_WORLD);
-
-    /* scatter number of elements each process will receive */
-    MPI_Scatter(nz_count, 1, MPI_INT, &nelements, 
-                1, MPI_INT, MASTER, MPI_COMM_WORLD);
-
-    /* allocate y vector */
-    y = (double *)calloc_or_exit( nrows, sizeof(double) );
-    if (rank != MASTER)
-        buf_x = (double *)malloc_or_exit( N * sizeof(double) );
-
-    /* allocate values, i_idx & j_idx arrays */
-    i_idx = (int *)malloc_or_exit( nelements * sizeof(int) );
-    j_idx = (int *)malloc_or_exit( nelements * sizeof(int) );
-    values = (double *)malloc_or_exit( nelements * sizeof(double));
+    res = mat_vec_mult_parallel(rank, nprocs, proc_info, buf_i_idx, 
+                                buf_j_idx, buf_values, buf_x);
     
-    debug("[%d] %d %d %d\n", rank, N, NZ, nelements);
-    
-    /* broadcast x vector */
-    /* TODO: change late to Point-2-Point communication */
-    MPI_Bcast(buf_x, N, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
-
-    /* scatter matrix elements to processes */
-    MPI_Scatterv(buf_values, nz_count, nz_start_idx, 
-                MPI_DOUBLE, values, nelements, MPI_DOUBLE, 
-                MASTER, MPI_COMM_WORLD);
-    MPI_Scatterv(buf_i_idx, nz_count, nz_start_idx, 
-                MPI_INT, i_idx, nelements, MPI_INT, 
-                MASTER, MPI_COMM_WORLD);
-    MPI_Scatterv(buf_j_idx, nz_count, nz_start_idx, 
-                MPI_INT, j_idx, nelements, MPI_INT, 
-                MASTER, MPI_COMM_WORLD);
-
-    /* validation check */
-    //for (int i = 0; i < nelements; i++) {
-    //    debug("[%d] %d %d %lf\n", rank, i_idx[i], j_idx[i], values[i]);
-    //}
-
-    /* perform the multiplication */
-    mat_vec_mult(values, i_idx, j_idx, buf_x, y, nelements, first_row);
-
-    /* gather y elements from processes */
-    MPI_Gatherv(y, nrows, MPI_DOUBLE, res, row_count, 
-                row_start_idx, MPI_DOUBLE, MASTER, MPI_COMM_WORLD);
-
     /* MPI: end */
     MPI_Finalize();
 
     /* write to output file */
-    if (rank == MASTER && out_file != NULL) {
-        printf("Writing result to '%s'\n", out_file);
+    if (rank == MASTER) {
+        if (out_file != NULL) {
+            printf("Writing result to '%s'\n", out_file);
 
-        /* open file */
-        FILE *f;
-        if ( !(f = fopen(out_file, "w")) ) {
-            fprintf(stderr, "fopen: failed to open file '%s'", out_file);
-            exit(EXIT_FAILURE);
+            /* open file */
+            FILE *f;
+            if ( !(f = fopen(out_file, "w")) ) {
+                fprintf(stderr, "fopen: failed to open file '%s'", out_file);
+                exit(EXIT_FAILURE);
+            }
+
+            /* write result */
+            for (int i = 0; i < proc_info[MASTER].N; i++) {
+                fprintf(f, "%.8lf\n", res[i]);
+            }
+            
+            /* close file */
+            if ( fclose(f) != 0) {
+                fprintf(stderr, "fopen: failed to open file '%s'", out_file);
+                exit(EXIT_FAILURE);
+            }
+
+            printf("Done!\n");
         }
 
-        /* write result */
-        for (int i = 0; i < N; i++) {
-            fprintf(f, "%.8lf\n",rank, res[i]);
-        }
-        
-        /* close file */
-        if ( fclose(f) != 0) {
-            fprintf(stderr, "fopen: failed to open file '%s'", out_file);
-            exit(EXIT_FAILURE);
-        }
-
-        printf("Done!\n");
+        /* free the memory */
+        free(buf_values);
+        free(buf_i_idx);
+        free(buf_j_idx);
+        free(buf_x);
+        free(res);
     }
-
-    /* free the memory */
-    free(values);
-    free(i_idx);
-    free(j_idx);
-    free(buf_x);
-    free(y);
-    
+        
     return 0;
 }
