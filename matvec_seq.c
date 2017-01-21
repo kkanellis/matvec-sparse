bbd49f590a889beedbc22149b04d2a17212c2eec
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-01-21 15:50:28 +0200

    change `matvec_seq` arguments to match parallel version

--- matvec_seq.c
+++ matvec_seq.c
@@ -22,7 +22,8 @@ void mat_vec_mult(const double *values,
 
 int main(int argc, char * argv[])
 {
-    char *filename;
+    char *in_file,
+         *out_file = NULL;
 
     double *values; /* a_values array */
     int *i_idx,     /* i_index array */
@@ -32,14 +33,18 @@ int main(int argc, char * argv[])
     int N, NZ;
 
     /* read arguments */
-    if (argc != 2) {
-        printf("Usage: %s filename\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        printf("Usage: %s input_file [output_file]\n", argv[0]);
         return 0;
     }
-    else filename = argv[1];
-    
+    else {
+        in_file = argv[1];
+        if (argc == 3) 
+            out_file = argv[2];
+    }
+
     /* read matrix */
-    if ( read_matrix(filename, &i_idx, &j_idx, &values, &N, &NZ) != 0) {
+    if ( read_matrix(in_file, &i_idx, &j_idx, &values, &N, &NZ) != 0) {
         exit(EXIT_FAILURE);
     }
 
@@ -54,21 +59,37 @@ int main(int argc, char * argv[])
     }
 
     /* generate random vector */
-    random_vec(x, N, MAX_RANDOM_NUM);
-    /*
+    //random_vec(x, N, MAX_RANDOM_NUM);
     for (int i = 0; i < N; i++) {
         x[i] = 1;
     }
-    */
     
     /* perform the multiplication */
     mat_vec_mult(values, i_idx, j_idx, x, y, NZ);
 
-    #ifdef DEBUG
-    for (int i = 0; i < N; i++) {
-        debug("%lf\n", y[i]);
+    if (out_file != NULL) {
+        printf("Writing result to '%s'\n", out_file);
+
+        /* open file */
+        FILE *f;
+        if ( !(f = fopen(out_file, "w")) ) {
+            fprintf(stderr, "fopen: failed to open file '%s'", out_file);
+            exit(EXIT_FAILURE);
+        }
+
+        /* write result */
+        for (int i = 0; i < N; i++) {
+            fprintf(f, "%.8lf\n", y[i]);
+        }
+        
+        /* close file */
+        if ( fclose(f) != 0) {
+            fprintf(stderr, "fopen: failed to open file '%s'", out_file);
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Done!\n");
     }
-    #endif
 
     /* free the memory */
     free(values);
